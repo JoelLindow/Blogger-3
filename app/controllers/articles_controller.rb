d9e050d0d09ff350fa0b8ef0442a5a07213696f2
JoelLindow <joellindow@gmail.com>
2017-08-27 13:28:24 -0600

    tagging completed with iteration 3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
 
   def index
     @articles = Article.all
@@ -19,13 +20,37 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+  @article.update(article_params)
 
-private
-
-def article_params
-  params.require(:article).permit(:title, :body)
+  redirect_to article_path(@article)
 end
+
+
+
+
+
+
+  def destroy
+    Article.destroy(params[:id])
+    redirect_to articles_path
+  end
+
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
-  @comment = Comment.new(comment_params)
-  @comment.article_id = params[:article_id]
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+    @comment.save
 
-  @comment.save
+    redirect_to article_path(@comment.article)
+  end
+  
+  private
 
-  redirect_to article_path(@comment.article)
-end
-
-private
-
-def comment_params
-  params.require(:comment).permit(:author_name, :body)
-end
-
-
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
 end
